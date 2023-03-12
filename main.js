10a67ce2b1bd824e1b8c0df05510d2c2c03168da
Daniela Benencio <daniela.benencio@gmail.com>
2023-03-12 03:08:16 -0300

    Agregar el contenido del carrito de compras y empezar con los condicionales para abrirlos y cerrarlos segun el responsive

--- main.js
+++ main.js
@@ -2,14 +2,23 @@ const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const burgerMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const carritoIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu )
 burgerMenu.addEventListener('click', toggleMobileMenu )
+carritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu () {
- desktopMenu.classList.toggle('inactive')
+ desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu () {
-    mobileMenu.classList.toggle('inactive')
-   }
+    mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains ('inactive');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    aside.classList.toggle('inactive')
+}
